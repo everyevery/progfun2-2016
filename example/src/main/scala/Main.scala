cfc336153186b9cd3928909c0e88d638140e0c71
Jongsoo Lee <leejongsoo@gmail.com>
2016-06-06 19:29:52 +0900

    add streams

---
+++ example/src/main/scala/Main.scala
@@ -0,0 +1,9 @@
+import example.Lists
+
+/**
+  * Created by pi on 2016. 5. 25..
+  */
+object Main extends App {
+  println(Lists.max(List(1,3,2)))
+
+}
---
+++ streams/src/main/scala/common/Pouring.scala
@@ -0,0 +1,67 @@
+package common
+
+/**
+  * Created by pi on 2016. 6. 5..
+  */
+class Pouring(capacity: Vector[Int]) {
+  type State = Vector[Int]
+  val initialState = capacity map (x => 0)
+
+  trait Move {
+    def change(state: State): State
+  }
+  case class Empty(glass: Int) extends Move {
+    def change(state: State) = state updated (glass, 0)
+  }
+  case class Fill(glass: Int) extends Move {
+    def change(state: State) = state updated (glass, capacity(glass))
+  }
+  case class Pour(from: Int, to: Int) extends Move {
+    def change(state: State) = {
+      val amount = state(from) min (capacity(to) - state(to))
+      state updated (from, state(from) - amount) updated (to, state(to) + amount)
+    }
+  }
+
+  val glasses = capacity.indices
+
+  val moves =
+    (for (g <- glasses) yield Empty(g)) ++
+      (for (g <- glasses) yield Fill(g)) ++
+      (for (from <- glasses; to <- glasses if from != to) yield Pour(from, to))
+
+  class Path(history: List[Move], val endState: State) {
+    def extend(move: Move) = new Path(move :: history, move change endState)
+    override def toString = (history.reverse mkString "") + "--> " + endState
+  }
+
+  val initialPath = new Path(Nil, initialState)
+
+  def from(paths: Set[Path], explored: Set[State]): Stream[Set[Path]] =
+    if (paths.isEmpty) Stream.empty
+    else {
+      val more = for {
+        path <- paths
+        next <- moves map path.extend
+        if !(explored contains next.endState)
+      } yield next
+      paths #:: from(more, explored ++ (more map (_.endState)))
+    }
+
+  val pathSets = from(Set(initialPath), Set(initialState))
+
+  def solutions(target: Int): Stream[Path] =
+    for {
+      pathSet <- pathSets
+      path <- pathSet
+      if path.endState contains target
+    } yield path
+}
+
+object Pouring {
+  def main(args: Array[String]): Unit = {
+    val problem = new Pouring(Vector(4, 9, 19))
+    problem.moves
+    println(problem.solutions(6))
+  }
+}
